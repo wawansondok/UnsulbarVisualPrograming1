41b214d838576a28b5c247ef9b030c0a0c15484c
wawansondok@gmail.com <101162509+wawansondok@users.noreply.github.com>
2022-03-29 22:29:28 +0800

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,13 @@
+class pemain:
+  pass
+   
+pemain1 = pemain()
+pemain2 = pemain();
+ 
+pemain1.olahraga = 'sepak bola'
+pemain1.gol = '30'
+ 
+pemain2.olahraga = 'sepak bola'
+pemain2.gol = '20'
+ 
+print(pemain1.__dict__)
