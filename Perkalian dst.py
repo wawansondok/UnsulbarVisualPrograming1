1d41da7578a47cd05d6b1ba6de0e1c77e35517a6
wawansondok@gmail.com <101162509+wawansondok@users.noreply.github.com>
2022-03-29 22:30:50 +0800

    Add files via upload

---
+++ Perkalian dst.py
@@ -0,0 +1,21 @@
+print ("============================")
+print ("         ARITMATIKA         ")
+print ("============================\n")
+
+
+#Kalau Perhitungan Artimatika sisa pake Aritmatika biasa
+
+a= int(input ("Angka1 :"))
+b= int(input("Angka2 :"))
+
+penjumlahan=a+b 
+pengurangan=a-b
+perkalian=a*b
+pembagian=a/b
+modulus=a%b
+
+print ("a+b=",penjumlahan)
+print ("a-b=",pengurangan)
+print ("a*b=",perkalian)
+print ("a/b=",pembagian)
+print ("a%b=",modulus)
