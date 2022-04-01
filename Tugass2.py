d9f49c3349bd02412f090b99992b0e709ce72328
wawansondok@gmail.com <101162509+wawansondok@users.noreply.github.com>
2022-04-01 15:31:13 +0800

    Add files via upload

---
+++ Tugass2.py
@@ -0,0 +1,9 @@
+class Binatang:
+    def __init__(self, inputNama, inputJumlah, inputWarna):
+        self.nama = inputNama
+        self.jumlah = inputJumlah
+        self.warna = inputWarna
+
+Rin1 = Binatang("Ayam",5,"Pink")
+Rin2 = Binatang("Kucing",8,"Bening")
+print(Rin1.jumlah)
